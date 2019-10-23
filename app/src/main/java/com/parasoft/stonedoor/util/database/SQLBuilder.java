b83532dc32fc7c8748d4958f6b72796fc1c81d94
Wojciech Powch <powch16@gmail.com>
2019-10-23 21:34:31 +0200

    SQLBuilder buildFieldsQuery

--- app/src/main/java/com/parasoft/stonedoor/util/database/SQLBuilder.java
+++ app/src/main/java/com/parasoft/stonedoor/util/database/SQLBuilder.java
@@ -1,9 +1,15 @@
 package com.parasoft.stonedoor.util.database;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class SQLBuilder {
     private StringBuilder queryStringBuilder;
+
+    private final String OPEN_QUOTE = "(";
+    private final String CLOSE_QUOTE = ")";
+    private final String COMMA = ", ";
+    private final String SPACE = " ";
 
     public String buildCreateTableQuery(String tableName, List<FieldConfig> fields) {
         try {
@@ -19,7 +25,18 @@ public class SQLBuilder {
     }
 
     private void buildFieldsQuery(List<FieldConfig> fields) {
-
+        final int lastIndex  = fields.size() - 1;
+        queryStringBuilder.append(OPEN_QUOTE);
+        IntStream.range(0, fields.size()).forEach( index -> {
+            FieldConfig fieldConfig = fields.get(index);
+            queryStringBuilder.append(fieldConfig.getFieldName());
+            queryStringBuilder.append(SPACE);
+            queryStringBuilder.append(fieldConfig.getFieldDataType());
+            if (index < lastIndex) {
+                queryStringBuilder.append(COMMA);
+            }
+        });
+        queryStringBuilder.append(CLOSE_QUOTE);
     }
 
     private void cleanQueryStringBuilder() {
